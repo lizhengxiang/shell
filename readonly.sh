9f5c18500be8d81374ff3df4d7604a92d64eace0
lizhengxiang <1065482100@qq.com>
2016-07-16 00:34:47 +0800

    shell readonly

---
+++ readonly.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+myUrl="shejiao520.com"
+readonly myUrl
+echo $myUrl
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo  "hello world !"
+your_name="www.shejiao520.com"
+echo $your_name
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
